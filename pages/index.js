28de1b44d985ee1066c06364404c0b5d33f06672
Tony Leeper <tonyleeper@blueyonder.co.uk>
2019-10-25 15:02:10 +0100

    styling components with css in js

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,62 @@ import Layout from '../components/MyLayout'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 
+const MovieLink = ({ show }) => (
+    <li key={show.id}>
+        <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+        </Link>
+        <style jsx>{`
+            li {
+                list-style: none;
+                margin: 5px 0;
+            }
+
+            a {
+                text-decoration: none;
+                color: blue;
+                font-family: 'Arial';
+            }
+
+            a:hover {
+                opacity: 0.6;
+            }
+        `}</style>
+    </li>
+)
+
 const Index = ({ shows }) => (
     <Layout>
         <h1>Batman TV Shows</h1>
         <ul>
             {shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
+                <MovieLink key={show.id} show={show} />
             ))}
         </ul>
+        <style jsx>{`
+            h1,
+            a {
+                font-family: 'Arial';
+            }
+
+            ul {
+                padding: 0;
+            }
+
+            li {
+                list-style: none;
+                margin: 5px 0;
+            }
+
+            a {
+                text-decoration: none;
+                color: blue;
+            }
+
+            a:hover {
+                opacity: 0.6;
+            }
+        `}</style>
     </Layout>
 )
 
